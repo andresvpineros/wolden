be2a65307e65408f08d7e8d33c4e671a3df73881
andresvpineros <andres.vpineros@gmail.com>
2022-06-09 19:07:01 -0500

    First commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import styles from './Navbar.module.scss';
+
+export default function Navbar() {
+  return (
+      <nav className={styles.navbar}>
+        <div className={styles['navbar__navbar-left']}>
+          <a className={styles['navbar__logo']} href="/" alt="Logo"><span className={styles['navbar__logo__first-letter']}>W</span>olden</a>
+        </div>
+        <div className={styles['navbar__navbar-center']}>
+          <div className={styles['searchbar']}>
+            <input type="text" className={styles['navbar__searchbar-input']} placeholder="Search in Wolden" />
+          </div>
+        </div>
+        <div className={styles['navbar__navbar-right']}>
+        {/* <NotificationIcon fill="currentColor" filled /> */}
+        </div>
+      </nav>
+  )
+}
